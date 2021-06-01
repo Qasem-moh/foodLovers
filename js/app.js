75aa3d254a54dda447193d33d5c6a69bc9d568f9
qasem-moh <2171246@s.aou.edu.jo>
2021-06-01 12:39:16 +0300

    final

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
 // define empty array for st all objects
 let FoodArray = [];
 
 //build constructor
 function Food(userName, type) {
-  this.type = "img/" + type + ".jpg";
+  this.type = "img/"+type+".jpg";
   this.userName = userName;
 
   FoodArray.push(this);
@@ -19,11 +19,11 @@ function RandomNumber() {
 
 //set data In localStorage
 function setDataInLS() {
-  localStorage.setItem("food", JSON.stringify(FoodArray));
+  localStorage.setItem('food', JSON.stringify(FoodArray));
 }
 //Get Data From localstorage
 function getDataFromLS() {
-  let result = JSON.parse(localStorage.getItem("food"));
+  let result = JSON.parse(localStorage.getItem('food'));
 
   if (result != null) {
     FoodArray = [];
@@ -36,7 +36,7 @@ function getDataFromLS() {
  */
 
 //get Data From user
-document.getElementById("FoodForm").addEventListener("submit", (event) => {
+document.getElementById('FoodForm').addEventListener('submit', (event) => {
   event.preventDefault();
 
   let user = event.target.userName.value;
@@ -51,64 +51,74 @@ console.log(FoodArray);
 //renderTable
 renderTable();
 function renderTable() {
-  let body = document.getElementById("resultFood");
+  let body = document.getElementById('resultFood');
 
-  let tableElement = document.createElement("table");
+  let tableElement = document.createElement('table');
   body.appendChild(tableElement);
 
-  let headerTable = document.createElement("thead");
+  let headerTable = document.createElement('thead');
   tableElement.appendChild(headerTable);
 
-  let trInThead = document.createElement("tr");
+  let trInThead = document.createElement('tr');
   headerTable.appendChild(trInThead);
 
-  let tdInHeaderuserName = document.createElement("td");
+  let tdInHeaderuserName = document.createElement('td');
   trInThead.appendChild(tdInHeaderuserName);
-  tdInHeaderuserName.textContent = "orderImg";
+  tdInHeaderuserName.textContent = 'orderImg';
 
-  let tdInHeaderTypeFood = document.createElement("td");
+  let tdInHeaderTypeFood = document.createElement('td');
   trInThead.appendChild(tdInHeaderTypeFood);
-  tdInHeaderTypeFood.textContent = "order Deatiels";
+  tdInHeaderTypeFood.textContent = 'order Deatiels';
 
-  let tbodyTable = document.createElement("tbody");
+  let tbodyTable = document.createElement('tbody');
   tableElement.appendChild(tbodyTable);
 }
 
 function renderDataInTable() {
   let count = 0;
-  let tbody = document.getElementsByTagName("tbody")[0];
-  tbody.textContent = "";
+  let tbody = document.getElementsByTagName('tbody')[0];
+  tbody.textContent = '';
   for (let i = 0; i < FoodArray.length; i++) {
-    let trInTbody = document.createElement("tr");
+    let trInTbody = document.createElement('tr');
     tbody.appendChild(trInTbody);
 
-    let tdinTbody = document.createElement("td");
+    let tdinTbody = document.createElement('td');
     trInTbody.appendChild(tdinTbody);
 
-    let imgTD = document.createElement("img");
-    imgTD.setAttribute("src", FoodArray[i].type);
+    let imgTD = document.createElement('img');
+    imgTD.setAttribute('src', FoodArray[i].type);
+    imgTD.setAttribute('indexArray', `${count}`);
+    imgTD.setAttribute('onclick', `removeItem(${count})`);
     tdinTbody.appendChild(imgTD);
 
     /*** */
-    let tdName = document.createElement("td");
+    let tdName = document.createElement('td');
     trInTbody.appendChild(tdName);
 
-    let pElement = document.createElement("p");
+    let pElement = document.createElement('p');
     tdName.appendChild(pElement);
-    pElement.textContent = "Customer name: " + FoodArray[i].userName;
+    pElement.textContent = 'Customer name: ' + FoodArray[i].userName;
     //************************ */
-    let pElement2 = document.createElement("p");
+    let pElement2 = document.createElement('p');
     tdName.appendChild(pElement2);
-    pElement2.textContent = "Food Type: " + FoodArray[i].type;
+    pElement2.textContent = "Food Type: " + `${FoodArray[i].type}`;
     /***************************** */
-    let pElement3 = document.createElement("p");
+    let pElement3 = document.createElement('p');
     tdName.appendChild(pElement3);
     // eslint-disable-next-line new-cap
     pElement3.textContent = RandomNumber();
   }
 }
 
+// eslint-disable-next-line no-unused-vars
+function removeItem(index) {
+  let newArray = FoodArray.filter((item) => item !== FoodArray[index]);
 
+  FoodArray = newArray;
+  setDataInLS();
+  getDataFromLS();
+  location.reload();
+}
 // let e3 = new Food("qasem", "pizza");
 getDataFromLS();
 renderDataInTable();
